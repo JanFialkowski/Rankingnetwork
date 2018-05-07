390d7661e2264e24f79a779d00ee1c654fa81c00
JanFialkowski <38782980+JanFialkowski@users.noreply.github.com>
2018-05-07 16:30:36 +0200

    Add files via upload

--- Mathingaround.py
+++ Mathingaround.py
@@ -2,38 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def BuildMatrix(Size=1000, a=1, b=1):
-    #baut die Wahrscheinlichkeitsmatrix für Size Knoten mit den Exponenten a und b.
-    Matrix = np.array([[float(i)+j for i in xrange(Size)] for j in xrange(Size)])
-    for i in xrange(len(Matrix)):
-        for j in xrange(len(Matrix[i])):
-            Matrix[i][j] = abs(i-j)**a/(i+j+2.)**b
-    #Matrix = Matrix.cumsum()
-    Matrix /= Matrix.sum()
-    #print Matrix.sum()
-    #Matrix = Matrix.reshape(len(Matrix),len(Matrix))
-    return Matrix
+def BuildProbas(Size=100, a=1, b=1):
+    Probs = [0 for i in xrange(Size)]
+    for i in xrange(Size):
+        print float(i)/Size*100
+        for j in xrange(Size):
+            Probs[i]+=float(abs(i-j)**a)/float((i+j+2)**b)
+    Probs = np.array(Probs)
+    Probs/=Probs.sum()
+    return Probs
                   
 
-def PlotProbs(NewMatrix, Plotlabel = ""):
-    #Plottet die kumulative Wahrscheinlichkeit für einen Knoten von Rang n Teil eines neuen Links zu sein.
-    CumProbs = [0 for i in xrange(len(NewMatrix))]
-    for i in xrange(len(NewMatrix)):
-        CumProbs[i]=NewMatrix[i].sum()
-        for j in xrange(len(NewMatrix)):
-            CumProbs[i] += NewMatrix[j][i]
+def PlotProbs(Probs, Plotlabel = ""):
+    CumProbs = Probs
     Xvalues= [float(i)/len(CumProbs)*100 for i in xrange(len(CumProbs))]
-    CumProbs = np.array(CumProbs)
-    CumProbs *= len(CumProbs)/float(100)
+    CumProbs *= float(len(CumProbs))/100.0
     plt.plot(Xvalues,CumProbs, label = Plotlabel)
     return
 
-NewMatrix = BuildMatrix(1000,1,1.6)
-PlotProbs(NewMatrix, Plotlabel="1000 Knoten")
-NewMatrix = BuildMatrix(100,1,1.6)
-PlotProbs(NewMatrix, Plotlabel = "100 Knoten")
+def FindMin(Probs):
+    MinProbs = 0
+    for i in xrange(len(Probs)):
+        if Probs[i]<Probs[MinProbs]:
+            MinProbs=i
+    return MinProbs
+
+Probs = BuildProbas(100,0,0.6)
+PlotProbs(Probs, Plotlabel="1 und 1.6")
+Probs = BuildProbas(100,100,100.6)
+PlotProbs(Probs, Plotlabel = "10 und 10.6")
 overx = np.array([1/(i+1)**0.6 for i in xrange(100)])
-overx /= overx.sum()*0.5
+overx /= overx.sum()
 plt.plot(overx, label="x**-0.6")
 plt.legend()
+plt.show()
+
+
+#to plot minima
+ass=[i/10.0 for i in xrange(11)]
+ProbList = []
+for value in ass:
+    ProbList.append(BuildProbas(a=value,b=value+0.6))
+yss=[]
+for Probs in ProbList:
+    yss.append(FindMin(Probs))
+plt.plot(ass,yss)
 plt.show()
