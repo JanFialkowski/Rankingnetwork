4ef0d0a54e714989eadbbd2bced2889613b23c7a
JanFialkowski <38782980+JanFialkowski@users.noreply.github.com>
2018-05-28 16:49:18 +0200

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -2,9 +2,25 @@ import graph_tool.all as gt
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
+import scipy.stats as sp
 
 filenames = sorted(glob.glob("./Realnetworks/tags*_2015.gml"))
 Graphs = [gt.load_graph(File) for File in filenames]
 
-plt.plot([len(Graph.get_vertices()) for Graph in Graphs])
+EperV = [gt.scalar_assortativity(Graph, "total") for Graph in Graphs]
+Values = [Thing[0] for Thing in EperV]
+plt.plot(Values)
+plt.plot([1,52],[-0.12,-0.12])
+plt.xlabel("Week")
+plt.ylabel("Assortativity r")
 plt.show()
+print sp.describe(Values)
+plt.hist(Values)
+plt.show()
+"""
+Summe = 0
+for value in Values:
+	Summe += value
+Summe /= len(EperV)
+print Summe
+"""
