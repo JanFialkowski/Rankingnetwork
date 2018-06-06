5040643dc5302870d405f429e73d175eac647395
JanFialkowski <38782980+JanFialkowski@users.noreply.github.com>
2018-06-06 11:41:43 +0200

    Add files via upload

--- Analysis.py
+++ Analysis.py
@@ -25,6 +25,27 @@ for Graph in Graphs:
 		if len(Score)<counting:
 			Score.append(0)
 print Scores
+
+#Ranking the nodes by their points, without age
+Ranks = [[] for i in LabeltoI.keys()]
+for i in xrange(len(Scores[0])):
+	Rankpoints = [ sl[i] for sl in Scores]
+	Rankings = len(Rankpoints) - sp.rankdata(Rankpoints, method = "ordinal").astype("float") + 1
+	for e in xrange(len(Ranks)):
+		Ranks[e].append(Rankings[e])
+
+Firstrankings = np.array([sl[0] for sl in Ranks])
+Firstscores = np.array([sl[0] for sl in Scores])
+actualranks = np.count_nonzero(Firstscores)
+print actualranks
+Firstrankings /= actualranks
+Firstscores *= actualranks
+Firstscores /= Firstscores.sum()
+print Firstrankings
+print Firstscores
+plt.scatter(Firstrankings, Firstscores)
+
+plt.show()
 """
 #Code-snippet to calcute graph wide average weight per node
 Graphweightsum = []
