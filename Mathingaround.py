8392fff8a0bc81f7a7ef5d0e569c1d2385f5a79b
JanFialkowski <38782980+JanFialkowski@users.noreply.github.com>
2018-06-14 12:47:50 +0200

    Add files via upload

--- Mathingaround.py
+++ Mathingaround.py
@@ -14,16 +14,31 @@ def BuildProbas(Size=100, a=1, b=1):
     return Probs
                   
 
-def PlotProbs(Probs, Plotlabel = ""):
+def PlotProbs(Probs, Plotlabel = "", lw=1.5):
     CumProbs = Probs
-    Xvalues= [float(i)/len(CumProbs)*100 for i in xrange(len(CumProbs))]
+    Xvalues= [float(i+1)/len(CumProbs)*100 for i in xrange(len(CumProbs))]
     CumProbs *= float(len(CumProbs))/100.0
-    plt.plot(Xvalues,CumProbs, label = Plotlabel)
+    plt.plot(Xvalues,CumProbs, label = Plotlabel, lw=lw)
     return
 
 def FindMin(Probs):
     return Probs.argmin()+1
 
+Probs = BuildProbas(100,1,1.6)
+PlotProbs(Probs, Plotlabel="a = 1 and b = 1.6")
+Probs = BuildProbas(100,2,2.6)
+PlotProbs(Probs, Plotlabel = "a= 2 and b = 2.6")
+Probs = BuildProbas(100,10,10.6)
+PlotProbs(Probs, Plotlabel = "a= 10 and b = 10.6")
+overx = np.array([1/(i+1)**0.6 for i in xrange(100)])
+overx /= overx.sum()
+plt.plot([i+1 for i in xrange(100)],overx, label=r"$x^{-0.6}$")
+plt.xlabel("Rank i")
+plt.ylabel(r"$w_{i}\cdot\left(\sum_{i=1}^{N}w_i\right)^{-1}$")
+plt.title(r"$w_{i}=\sum_{j=1}^{100}\frac{\vert i - j\vert^{a}}{(i + j)^{a + 0.6}}$", y=1.025)
+plt.legend()
+plt.show()
+"""
 somelist = []
 for i in xrange(6):
 	a=1
@@ -37,35 +52,37 @@ plt.xlabel("Rank i")
 plt.ylabel(r"$w_{i}\cdot\left(\sum_{i=1}^{100}w_i\right)^{-1}$")
 plt.show()
 """
+#"""
 #rc("text", usetex=True)
 Probs = BuildProbas(100,1,1.6)
-PlotProbs(Probs, Plotlabel="N = 100 Ranks")
-Probs = BuildProbas(100,1,1.6)
+PlotProbs(Probs, Plotlabel="N = 100 Ranks", lw=2.5)
+Probs = BuildProbas(10000,1,1.6)
 PlotProbs(Probs, Plotlabel = "N = 10000 Ranks")
 overx = np.array([1/(i+1)**0.6 for i in xrange(100)])
 overx /= overx.sum()
 plt.plot([i+1 for i in xrange(100)],overx, label=r"$x^{-0.6}$")
-plt.xlabel(r"$R_i\cdot \frac{100}{N}$")
-plt.ylabel(r"$\frac{\sum_{j=1}^{N} w_{ij}}{\sum_{i,j=1}^{N} w_{ij}}\cdot \frac{N}{100}$", fontsize = 15)
+plt.xlabel(r"$i\cdot \frac{100}{N}$")
+plt.ylabel(r"$w_{i}\cdot\left(\sum_{i=1}^{N}w_i\right)^{-1}\cdot\frac{N}{100}$")
+plt.title(r"$w_{i}=\sum_{j=1}^{N}\frac{\vert i - j\vert^1}{(i + j)^{1.6}}$", y=1.025)
 plt.legend()
 plt.show()
+#"""
 """
-"""
-Iss = range(51)
+Iss = range(1001)
 MinRanks = []
 Xss = []
 for i in Iss:
-    Xss.append(i/25.0)
-    Probs = BuildProbas(100,1,1+i/25.0)
+    Xss.append(i/500.0)
+    Probs = BuildProbas(500,1,1+i/500.0)
     MinRanks.append(FindMin(Probs))
-plt.plot(Xss,MinRanks,label=r"$\min_i \sum_{j=1}^{100}\frac{\vert i - j \vert^1}{(i + j)^{1+x}}$")
+plt.plot(Xss,MinRanks,label=r"$\min_i \sum_{j=1}^{500}\frac{\vert i - j \vert^1}{(i + j)^{1+x}}$")
 MinRanks = []
 Xss = []
 for i in Iss:
-    Xss.append(i/25.0)
-    Probs = BuildProbas(100,6,6+i/25.0)
+    Xss.append(i/500.0)
+    Probs = BuildProbas(500,6,6+i/500.0)
     MinRanks.append(FindMin(Probs))
-plt.plot(Xss,np.array(MinRanks),label=r"$\min_i \sum_{j=1}^{100}\frac{\vert i - j \vert^6}{(i + j)^{6+x}}$")
+plt.plot(Xss,np.array(MinRanks),label=r"$\min_i \sum_{j=1}^{500}\frac{\vert i - j \vert^6}{(i + j)^{6+x}}$")
 plt.xlabel("Difference of the exponents x")
 plt.ylabel("Rank with lowest probability")
 plt.legend(fontsize = 12)
